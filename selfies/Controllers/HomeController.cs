4a0d98aaeb243cfb51260cd2d3e2a3ccd3107685
Seth Hayward <seth.hayward@gmail.com>
2013-11-18 09:24:42 -0800

    rebuild everything with vs2012, added db connect

---
+++ selfies/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using selfies.Models;
+
+namespace selfies.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private selfiesMySQL _db;
+        public selfiesMySQL db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    _db = new selfiesMySQL();
+                }
+                return _db;
+            }
+            set
+            {
+                _db = value;
+            }
+        }
+
+
+        public ActionResult Index()
+        {
+            ViewBag.selfiesCount = db.selfies.Count();
+            return View();
+        }
+    }
+}
