fde0c077b95c2fef6bba408923f6e1dbd64155d0
Seth Hayward <seth.hayward@gmail.com>
2013-11-19 18:37:59 -0800

    working upload, kinda

---
+++ selfies/Controllers/UploadController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using selfies.Models;
+
+namespace selfies.Controllers
+{
+    public class UploadController : ApiController
+    {
+
+        private selfiesMySQL _db;
+        public selfiesMySQL db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    _db = new selfiesMySQL();
+                }
+                return _db;
+            }
+            set
+            {
+                _db = value;
+            }
+        }
+
+
+        public async Task<HttpResponseMessage> PostFile()
+        {
+            // Check if the request contains multipart/form-data.
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            string root = HttpContext.Current.Server.MapPath("~/App_Data/");
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+            {
+                StringBuilder sb = new StringBuilder(); // Holds the response body
+
+                // Read the form data and return an async task.
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                // This illustrates how to get the form data.
+                foreach (var key in provider.FormData.AllKeys)
+                {
+                    foreach (var val in provider.FormData.GetValues(key))
+                    {
+                        sb.Append(string.Format("{0}: {1}\n", key, val));
+                    }
+                }
+
+                // This illustrates how to get the file names for uploaded files.
+                foreach (var file in provider.FileData)
+                {
+                    FileInfo fileInfo = new FileInfo(file.LocalFileName);
+                    sb.Append(string.Format("Uploaded file: {0} ({1} bytes)\n", fileInfo.Name, fileInfo.Length));
+
+                    selfy self = new selfy();
+
+                    self.dateCreated = DateTime.UtcNow;
+                    self.selfieGuid = System.Guid.NewGuid().ToString();
+                    self.userGuid = "1";
+                    db.selfies.Add(self);
+                    db.SaveChanges();
+
+                    string dir_path = HttpContext.Current.Server.MapPath("~/App_Data/selfies/");
+
+                    if (Directory.Exists(dir_path + "selfies") == false)
+                    {
+                        Directory.CreateDirectory(dir_path);
+                    }
+                    fileInfo.CopyTo(dir_path + self.selfieGuid);
+                    fileInfo.Delete();
+                    
+
+                }
+
+                var response = Request.CreateResponse(HttpStatusCode.Moved);
+                string fullyQualifiedUrl = Request.RequestUri.GetLeftPart(UriPartial.Authority);
+                response.Headers.Location = new Uri(fullyQualifiedUrl);
+                return response;
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+
+    }
+}
