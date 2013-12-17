04aadfa461c2357062c67d3bb05f2c024e186bf7
Seth Hayward <seth.hayward@gmail.com>
2013-12-16 16:49:07 -0800

    initial api for allowing handle creation from the web app

--- selfies/Controllers/HandleController.cs
+++ selfies/Controllers/HandleController.cs
@@ -4,26 +4,66 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using selfies.Models;
 
 namespace selfies.Controllers
 {
     public class HandleController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+
+        private selfiesMySQL _db;
+        public selfiesMySQL db
         {
-            return new string[] { "value1", "value2" };
+            get
+            {
+                if (_db == null)
+                {
+                    _db = new selfiesMySQL();
+                }
+                return _db;
+            }
+            set
+            {
+                _db = value;
+            }
+        }
+
+
+        // GET api/values
+        public IEnumerable<handle> Get()
+        {
+            return (from m in db.handles select m).ToList();
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public handle Get(int id)
         {
-            return "value";
+            handle selected = (from m in db.handles where m.id.Equals(id) select m).FirstOrDefault();
+            return selected;
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public void Post(handle value)
         {
+
+            if (value != null)
+            {
+                handle currently_exists = (from m in db.handles where m.name.Equals(value.name) select m).FirstOrDefault();
+
+                if (currently_exists == null)
+                {
+
+                    handle safe_handle = new handle();
+                    Guid guid = (Guid)System.Web.Security.Membership.GetUser().ProviderUserKey;
+                    safe_handle.userGuid = guid.ToString();
+                    safe_handle.active = 1;
+                    // free handle yay
+                    db.handles.Add(safe_handle);
+                    db.SaveChanges();
+                }
+
+            }
+
         }
 
         // PUT api/values/5
