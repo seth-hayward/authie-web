00cddc9733701da58c8a9c99ff4e6077c032b34d
Seth Hayward <seth.hayward@gmail.com>
2014-01-03 09:12:51 -0800

    move away from the template login forms toward my stuff

--- selfies/Controllers/AccountController.cs
+++ selfies/Controllers/AccountController.cs
@@ -20,49 +20,16 @@ namespace MvcApplication1.Controllers
         }
 
         //
-        // GET: /Account/LogOn
-
-        public ActionResult LogOn()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Account/LogOn
-
-        [HttpPost]
-        public ActionResult LogOn(LogOnModel model, string returnUrl)
-        {
-            if (ModelState.IsValid)
-            {
-                if (Membership.ValidateUser(model.UserName, model.Password))
-                {
-                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
-                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
-                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
-                    {
-                        return Redirect(returnUrl);
-                    }
-                    else
-                    {
-                        return RedirectToAction("Index", "Home");
-                    }
-                }
-                else
-                {
-                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
-        //
         // GET: /Account/LogOff
 
         public ActionResult LogOff()
         {
+
+            // DELETE / DEACTIVATE THE ACCOUNT
+
+            // or maybe just show them the first 8 characters or whatever
+            // of their privateKey so they can log back in?
+
             FormsAuthentication.SignOut();
 
             return RedirectToAction("Index", "Home");
