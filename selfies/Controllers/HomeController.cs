94310dd939146ece65ff35adcea33dcdd16ff303
Seth Hayward <seth.hayward@gmail.com>
2014-01-04 15:30:50 -0800

    this bug was throwing issues with users that were not logged in

--- selfies/Controllers/HomeController.cs
+++ selfies/Controllers/HomeController.cs
@@ -41,6 +41,8 @@ namespace selfies.Controllers
 
         public ActionResult Details(string handle)
         {
+
+
             string user_id = User.Identity.Name;
             handle logged_in = (from handle r in db.handles where r.userGuid.Equals(User.Identity.Name) select r).FirstOrDefault();
             ViewBag.handle = logged_in;
@@ -48,20 +50,23 @@ namespace selfies.Controllers
             handle selected_handle = (from handle r in db.handles where r.name.Equals(handle) && r.active == 1 select r).FirstOrDefault();
             ViewBag.selectedHandle = selected_handle;
 
-            if (logged_in.publicKey == selected_handle.publicKey)
+            ViewBag.addEdit = false;
+            if (logged_in != null)
             {
 
-                ViewBag.addEdit = true;
-                // own profile
-                if (logged_in.tagLine == null)
+                if (logged_in.publicKey == selected_handle.publicKey)
                 {
-                    logged_in.tagLine = "click to add a tagline";
+
+                    ViewBag.addEdit = true;
+                    // own profile
+                    if (logged_in.tagLine == null)
+                    {
+                        logged_in.tagLine = "click to add a tagline";
+                    }
                 }
+
             }
-            else
-            {
-                ViewBag.addEdit = false;
-            }
+
 
             List<thread> public_threads = (from thread m in db.threads where m.fromHandleId.Equals(selected_handle.publicKey) && m.toHandleId.Equals("1") select m).ToList();
             ViewData.Model = public_threads;
