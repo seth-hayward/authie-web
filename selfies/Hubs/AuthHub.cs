55b12897398ddc001f32d46ccdf504594bd8ab2f
Seth Hayward <seth.hayward@gmail.com>
2014-01-23 08:59:46 -0800

    change names

--- selfies/Hubs/AuthHub.cs
+++ selfies/Hubs/AuthHub.cs
@@ -11,7 +11,7 @@ namespace selfies.Hubs
         public void Send(string name, string message)
         {
             // Call the addNewMessageToPage method to update clients.
-            Clients.All.addNewMessageToPage(name, message);
+            Clients.All.addMessage(name, message);
         }
     }
 }
