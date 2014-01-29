60f752dc8a73d6f574c8006623f7b0606cdf35fd
Seth Hayward <seth.hayward@gmail.com>
2014-01-29 10:24:51 -0800

    create new images if they don't exist

--- selfies/Controllers/SnapController.cs
+++ selfies/Controllers/SnapController.cs
@@ -15,15 +15,51 @@ namespace selfies.Controllers
 
         public HttpResponseMessage Get(string id, string type)
         {
+            string orig = HttpContext.Current.Server.MapPath("~/snaps/orig/");
             string root = HttpContext.Current.Server.MapPath("~/snaps/" + type + "/");
             var path = root + id;
+            var orig_path = orig + id;
 
             HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
 
             if (File.Exists(path) == false)
             {
-                return new HttpResponseMessage
-                ();
+
+                // try regenerating it if the original file exists
+                if(File.Exists(orig_path) == true) {
+
+                    FileInfo fileInfo = new FileInfo(orig_path);
+
+                    // generate 640px version (retina display)
+                    string sz640_dir = HttpContext.Current.Server.MapPath("~/snaps/640/");
+                    string filename = sz640_dir + id;
+                    ImageResizer.ImageJob i = new ImageResizer.ImageJob(orig_path, filename, new ImageResizer.Instructions(
+                                    "width=640;height=900;format=jpg;mode=max;autorotate=true"));
+                    i.CreateParentDirectory = true; //Auto-create the uploads directory.
+                    i.Build();
+
+                    // generate a 500px version
+                    string sz500_dir = HttpContext.Current.Server.MapPath("~/snaps/500/");
+                    filename = sz500_dir + id;
+                    i = new ImageResizer.ImageJob(orig_path, filename, new ImageResizer.Instructions(
+                                    "width=500;height=900;format=jpg;mode=max;autorotate=true"));
+                    i.CreateParentDirectory = true; //Auto-create the uploads directory.
+                    i.Build();
+
+
+                    // generate thumb version
+                    string thumb_dir = HttpContext.Current.Server.MapPath("~/snaps/thumb/");
+                    filename = thumb_dir + id;
+                    i = new ImageResizer.ImageJob(orig_path, filename, new ImageResizer.Instructions(
+                                    "width=75;height=75;format=jpg;mode=max;autorotate=true"));
+                    i.CreateParentDirectory = true; //Auto-create the uploads directory.
+                    i.Build();
+
+                } else {
+                    // doesn't exist.. so... do nothing
+                    return new HttpResponseMessage();
+                }
+
             }
 
             var stream = new FileStream(path, FileMode.Open);
