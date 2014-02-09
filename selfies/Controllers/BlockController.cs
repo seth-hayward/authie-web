cd69398b164fe7ea232c571f9217120e50c8b046
Seth Hayward <seth.hayward@gmail.com>
2014-02-09 00:10:58 -0800

    when blocking, remove the contact from both users

--- selfies/Controllers/BlockController.cs
+++ selfies/Controllers/BlockController.cs
@@ -54,12 +54,8 @@ namespace selfies.Controllers
                 b.blockedHandleId = blocked_user.id;
                 b.blockedOn = DateTime.UtcNow;
                 db.blocks.Add(b);
-                db.SaveChanges();
 
                 // remove original contact if it exists
-                //handle to_handle = (from m in db.handles where m.id == remove_id && m.active == 1 select m).FirstOrDefault();
-                //handle from_handle = (from handle r in db.handles where r.userGuid.Equals(User.Identity.Name) select r).FirstOrDefault();
-
                 follower follow_connect = (from m in db.followers where m.followerHandle.id == logged_in.id && m.followeeHandle.id == blocked_user.id && m.active == 1 select m).FirstOrDefault();
                 if (follow_connect != null)
                 {
@@ -69,34 +65,45 @@ namespace selfies.Controllers
 
                     var updated_follower = db.Entry(follow_connect);
                     updated_follower.Property(e => e.active).IsModified = true;
-
-                    db.SaveChanges();
-
-                    string password = System.Web.Configuration.WebConfigurationManager.AppSettings["MailPassword"];
-
-                    MailMessage Message = new MailMessage();
-                    SmtpClient Smtp = new SmtpClient();
-
-                    System.Net.NetworkCredential SmtpUser = new System.Net.NetworkCredential("noreply@letterstocrushes.com", password);
-
-                    string email = logged_in.name + " blocked " + blocked_user.name;
-
-                    Message.From = new MailAddress("hello@selfies.io");
-                    Message.To.Add(new MailAddress("seth.hayward@gmail.com"));
-                    Message.IsBodyHtml = false;
-                    Message.Subject = "block";
-                    Message.Body = email;
-                    Message.Priority = MailPriority.Normal;
-                    Smtp.EnableSsl = false;
-
-                    Smtp.Credentials = SmtpUser;
-                    Smtp.Host = "198.57.199.92";
-                    Smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-                    Smtp.Port = 26;
-
-                    Smtp.Send(Message);
-
                 }
+
+                // remove the other person's contact as well
+                follower blocked_connect = (from m in db.followers where m.followerHandle.id == blocked_user.id && m.followeeHandle.id == logged_in.id && m.active == 1 select m).FirstOrDefault();
+                if (blocked_connect != null)
+                {
+                    blocked_connect.active = 0;
+
+                    db.followers.Attach(blocked_connect);
+
+                    var updated_block_follower = db.Entry(blocked_connect);
+                    updated_block_follower.Property(e => e.active).IsModified = true;
+                }
+
+                db.SaveChanges();
+
+                string password = System.Web.Configuration.WebConfigurationManager.AppSettings["MailPassword"];
+
+                MailMessage Message = new MailMessage();
+                SmtpClient Smtp = new SmtpClient();
+
+                System.Net.NetworkCredential SmtpUser = new System.Net.NetworkCredential("noreply@letterstocrushes.com", password);
+
+                string email = logged_in.name + " blocked " + blocked_user.name;
+
+                Message.From = new MailAddress("hello@selfies.io");
+                Message.To.Add(new MailAddress("seth.hayward@gmail.com"));
+                Message.IsBodyHtml = false;
+                Message.Subject = "block";
+                Message.Body = email;
+                Message.Priority = MailPriority.Normal;
+                Smtp.EnableSsl = false;
+
+                Smtp.Credentials = SmtpUser;
+                Smtp.Host = "198.57.199.92";
+                Smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                Smtp.Port = 26;
+
+                Smtp.Send(Message);
 
 
 
